cb62a4b9309e345e7ee7581a44d12436bf608913
Seraph <seraph.axl@gmail.com>
2025-05-28 07:27:37 -0600

    Reemplazo de claves de Supabase por variables de entorno

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,2 @@
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://acfmhybvlbqotkjjxjjd.supabase.co'
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjZm1oeWJ2bGJxb3Rramp4ampkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxODA5OTEsImV4cCI6MjA2Mzc1Njk5MX0.5CNRnCmBD24k3ojVnxnE7TKfwIrmoen90wE9j68DqFc'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
