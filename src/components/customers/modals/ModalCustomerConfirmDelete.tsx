ab3f88faf486c67b6b427e9791f696bf8b550f92
Seraph <seraph.axl@gmail.com>
2025-07-02 03:57:45 -0600

    feat: actualizar modal de confirmación de eliminación de cliente para mostrar el nombre del cliente y mejorar mensajes de advertencia

--- src/components/customers/modals/ModalCustomerConfirmDelete.tsx
+++ src/components/customers/modals/ModalCustomerConfirmDelete.tsx
@@ -10,23 +10,25 @@ interface ModalSelectCustomerProps {
 }
 
 const ModalCustomerConfirmDelete = ({ isOpen, onOpenChange, onConfirm }: ModalSelectCustomerProps) => {
-  const quote = useSelector((state: RootState) => state.quote.data)
+  const customer = useSelector((state: RootState) => state.clientes.selectedCustomer)
   return (
     <Modal isOpen={isOpen} onOpenChange={onOpenChange} size='sm' backdrop='opaque'>
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className='flex flex-col gap-1'>{quote.status === 'open' ? 'Eliminar' : 'Archivar'} cotización</ModalHeader>
+            <ModalHeader className='flex flex-col gap-1'>Eliminar cliente</ModalHeader>
             <ModalBody>
-              <p>¿Estás seguro que deseas eliminar al cliente {'cliente'}?</p>
+              <p>
+                ¿Estás seguro que deseas eliminar al cliente <span className='font-semibold'>{customer?.name}</span>?
+              </p>
 
               <Alert
                 classNames={{ alertIcon: 'fill-none', base: 'p-1' }}
                 hideIconWrapper
                 color='danger'
                 icon={<TriangleAlert />}
-                title='Advertencia'
-                description='¡Atención! Esta acción eliminará los datos del cliente, pero conservara el historial de cotizaciones. Esto no se puede deshacer'
+                title='¡Atención! Esta acción es irreversible'
+                description='Se eliminarán los datos del cliente, pero se conservará el historial de cotizaciones.'
               />
             </ModalBody>
             <ModalFooter>
