8b4a80df6f1a795b4b21f19bcebb1d55ddfc5902
Seraph <seraph.axl@gmail.com>
2025-07-03 06:48:37 -0600

    Agregar under construction

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,12 @@
 const Users = () => {
-  return <div>Users</div>
+  return (
+    <div className='min-h-screen w-full flex justify-center items-center bg-[url("/utils/under-construction.webp")] bg-center'>
+      <div className='bg-white bg-opacity-80 text-center fixed bottom-20'>
+        <h1 className='text-3xl font-bold mb-4'>Página en Construcción</h1>
+        <p className='text-gray-600'>Estamos trabajando para traerte una mejor experiencia. ¡Vuelve pronto!</p>
+      </div>
+    </div>
+  )
 }
 
 export default Users
