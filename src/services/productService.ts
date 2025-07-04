a4e9bb465622d2ce7e20f7e742cba3082a4078f5
Seraph <seraph.axl@gmail.com>
2025-07-04 02:45:21 -0600

    feat: agregar funciones para manejar elementos del catálogo (agregar, eliminar, actualizar)

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,6 +1,6 @@
 import { addToast } from '@heroui/react'
 import { supabase } from '../lib/supabase'
-import { Product } from '../types'
+import { Category, MeasureUnit, Product, Provider } from '../types'
 
 export const productService = {
   async getProducts(): Promise<Product[]> {
@@ -105,6 +105,164 @@ export const productService = {
       })
 
       console.error('Error:', err)
+    }
+  },
+  async addCatalogItem(
+    type: 'provider' | 'category' | 'measureUnit',
+    item: Provider | Category | MeasureUnit
+  ): Promise<Provider | Category | MeasureUnit | null> {
+    try {
+      let tableName: string
+      let data: Provider | Category | MeasureUnit
+      let itemName: string
+
+      switch (type) {
+        case 'provider':
+          itemName = 'Proveedor'
+          tableName = 'providers'
+          data = { name: (item as Provider).name }
+          break
+        case 'category':
+          itemName = 'Categoría'
+          tableName = 'categories'
+          data = { description: (item as Category).description, color: (item as Category).color }
+          break
+        case 'measureUnit':
+          itemName = 'Unidad de medida'
+          tableName = 'measure_units'
+          data = { key: (item as MeasureUnit).key, name: (item as MeasureUnit).name, plural: (item as MeasureUnit).plural }
+          break
+        default:
+          throw new Error('Tipo de catálogo no reconocido')
+      }
+
+      const { data: newItem, error } = await supabase.from(tableName).insert(data).select().single()
+      if (error) throw error
+
+      addToast({
+        title: `${itemName} ${itemName === 'Proveedor' ? 'agregado' : 'agregada'}`,
+        description: `${itemName === 'Proveedor' ? 'El' : 'La'} ${itemName.toLowerCase()}  se ha guardado correctamente.`,
+        color: 'success'
+      })
+
+      return newItem
+    } catch (err) {
+      addToast({
+        title: 'Error al guardar',
+        description: 'Hubo un error al guardar el ítem. Inténtalo de nuevo.',
+        color: 'danger'
+      })
+
+      console.error('Error:', err)
+      return null
+    }
+  },
+  async deleteCatalogItem(type: 'provider' | 'category' | 'measureUnit', item: Provider | Category | MeasureUnit): Promise<void> {
+    try {
+      let tableName: string
+      let itemId: number | string | undefined
+      let itemName: string
+
+      console.log('tipo:', type, 'item:', item)
+
+      switch (type) {
+        case 'provider':
+          itemName = 'Proveedor'
+          tableName = 'providers'
+          itemId = (item as Provider).id
+          break
+        case 'category':
+          itemName = 'Categoría'
+          tableName = 'categories'
+          itemId = (item as Category).id
+          break
+        case 'measureUnit':
+          itemName = 'Unidad de medida'
+          tableName = 'measure_units'
+          itemId = (item as MeasureUnit).key
+          break
+        default:
+          throw new Error('Tipo de catálogo no reconocido')
+      }
+
+      const { error } = await supabase
+        .from(tableName)
+        .delete()
+        .eq(type === 'measureUnit' ? 'key' : 'id', itemId)
+      if (error) throw error
+
+      addToast({
+        title: `${itemName} ${itemName === 'Proveedor' ? 'eliminado' : 'eliminada'}`,
+        description: `${itemName === 'Proveedor' ? 'El' : 'La'} ${itemName.toLowerCase()} se ha eliminado correctamente.`,
+        color: 'success'
+      })
+    } catch (err) {
+      addToast({
+        title: 'Error al eliminar',
+        description: 'Hubo un error al eliminar el ítem. Inténtalo de nuevo.',
+        color: 'danger'
+      })
+
+      console.error('Error:', err)
+    }
+  },
+  async updateCatalogItem(
+    type: 'provider' | 'category' | 'measureUnit',
+    item: Provider | Category | MeasureUnit
+  ): Promise<Provider | Category | MeasureUnit | null> {
+    try {
+      let tableName: string
+      let data: Provider | Category | MeasureUnit
+      let itemId: number | string | undefined
+      let itemName: string
+
+      switch (type) {
+        case 'provider':
+          itemName = 'Proveedor'
+          tableName = 'providers'
+          data = { name: (item as Provider).name }
+          itemId = (item as Provider).id
+          break
+        case 'category':
+          itemName = 'Categoría'
+          tableName = 'categories'
+          data = { description: (item as Category).description, color: (item as Category).color }
+          itemId = (item as Category).id
+          break
+        case 'measureUnit':
+          itemName = 'Unidad de medida'
+          tableName = 'measure_units'
+          data = { key: (item as MeasureUnit).key, name: (item as MeasureUnit).name, plural: (item as MeasureUnit).plural }
+          itemId = (item as MeasureUnit).key
+          break
+        default:
+          throw new Error('Tipo de catálogo no reconocido')
+      }
+
+      const { data: updatedItem, error } = await supabase
+        .from(tableName)
+        .update(data)
+        .eq(type === 'measureUnit' ? 'key' : 'id', itemId)
+        .select()
+        .single()
+      if (error) throw error
+
+      addToast({
+        title: `${itemName} ${itemName === 'Proveedor' ? 'eliminado' : 'eliminada'} `,
+        description: `${itemName === 'Proveedor' ? 'El' : 'La'} ${itemName.toLowerCase()} se ha actualizado correctamente.`,
+        color: 'primary'
+      })
+
+      return updatedItem
+    } catch (err) {
+      addToast({
+        title: 'Error al actualizar',
+        description: 'Hubo un error al actualizar el ítem. Inténtalo de nuevo.',
+        color: 'danger'
+      })
+
+      console.error('Error:', err)
+      return null
     }
   }
 }
