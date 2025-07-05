7cba122240d895d1be8a32487fafb259e4b0758d
Seraph <seraph.axl@gmail.com>
2025-07-05 14:17:00 -0600

    fix: hacer opcional el campo package_unit y corregir mensaje de error para proveedor

--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -9,9 +9,8 @@ export const productSchema = z.object({
     return val
   }, z.string().optional()),
   provider: z.coerce.number().nonnegative(),
-  package_unit: z.preprocess((val) => (val === '' ? undefined : val), z.coerce.number().nonnegative()).default(1),
+  package_unit: z.preprocess((val) => (val === '' ? undefined : val), z.coerce.number().nonnegative().default(1)).optional(),
   measurement_unit: z.string().min(1, 'La unidad de medida es requerida'),
-
   price: z.coerce.number().nonnegative().positive(),
   utility: z.coerce.number().nonnegative().positive(),
   category: z.coerce.number().nonnegative(),
@@ -38,16 +37,33 @@ export const productSchemaAdd = productSchema.extend({
       })
       .nonnegative()
   ),
-
   provider: z.preprocess(
     (val) => (val === '' ? undefined : Number(val)),
     z
       .number({
-        required_error: 'La categoría es requerida',
+        required_error: 'El proveedor es requerido',
         invalid_type_error: 'Selecciona un proveedor'
       })
       .nonnegative()
   )
 })
 
+// Tipos derivados
 export type ProductFormData = z.infer<typeof productSchema>
+export type ProductFormDataUpdate = z.infer<typeof productSchemaUpdate>
+
+export type ProductFormDataClean = {
+  sku: string
+  description?: string
+  provider: number
+  package_unit?: number
+  measurement_unit: string
+  price: number
+  utility: number
+  category: number
+  spec?: string
+}
+
+export type ProductFormDataUpdateClean = ProductFormDataClean & {
+  id?: number
+}
