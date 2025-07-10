e69692a29b630a5485a4651b101b083c4c182fc6
Seraph <seraph.axl@gmail.com>
2025-07-10 08:17:56 -0600

    feat: agregar carga de usuarios en el componente AppStart

--- src/AppStart.tsx
+++ src/AppStart.tsx
@@ -3,6 +3,7 @@ import { useCustomers } from './hooks/useCustomers'
 import { useMeasureUnits } from './hooks/useMeasureUnits'
 import { useProducts } from './hooks/useProducts'
 import { useProviders } from './hooks/useProviders'
+import { useUsers } from './hooks/useUsers'
 
 export const AppStart = () => {
   useCategories() //Carga de categorias desde supabase a redux
@@ -10,5 +11,6 @@ export const AppStart = () => {
   useMeasureUnits() //Carga de unidades de medida desde supabase a redux
   useCustomers() //Carga de clientes desde supabase a redux
   useProducts() //Carga de productos desde supabase a redux
+  useUsers() //Carga de usuarios desde supabase a redux
   return null // No renderiza nada
 }
