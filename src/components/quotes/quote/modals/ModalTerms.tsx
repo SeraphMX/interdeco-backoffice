6fd69ab8bc4e4eb53ac981860831a12375e14991
Edgar Moreira Ortiz <seraph.axl@gmail.com>
2025-07-13 13:41:22 -0600

    IDEC-85-agregar-descuentos-en-la-cotizacion-pdf (#43)
    * feat: mejorar formato de cotización y agregar manejo de descuentos en QuotePDF
    
    * fix: actualizar vigencia de la cotización a 7 días y eliminar texto redundante en ModalTerms

--- src/components/quotes/quote/modals/ModalTerms.tsx
+++ src/components/quotes/quote/modals/ModalTerms.tsx
@@ -13,19 +13,13 @@ const ModalTerms = ({ isOpen, onOpenChange }: ModalTermsProps) => {
             <ModalHeader className='flex flex-col gap-1'>Términos de la cotización</ModalHeader>
             <ModalBody>
               <ul className='text-sm text-gray-600 list-disc list-inside space-y-1 '>
-                <li>Vigencia de la cotización 3 días naturales</li>
+                <li>Vigencia de la cotización 7 días naturales</li>
                 <li>Precios sujetos a cambio sin previo aviso y sujetos a existencia.</li>
                 <li>Una vez realizado el pedido no se aceptan cambios de material ni cancelaciones.</li>
                 <li>Los tiempos de entrega varían dependiendo del material.</li>
                 <li>Mejoramos cualquier presupuesto presentado por escrito.</li>
                 <li>Trabajo 100% garantizado.</li>
               </ul>
-
-              <p className='text-sm'>
-                En <strong>InterDeco</strong>fiamos tanto en la calidad y el valor de nuestro servicio, que te ofrecemos una{' '}
-                <strong>garantía única</strong>: Si encuentras un presupuesto más bajo (por escrito y con los mismos materiales
-                especificados) <strong>igualamos el precio</strong> y <strong>te regalamos la instalación</strong>.
-              </p>
             </ModalBody>
             <ModalFooter>
               <Button variant='light' color='primary' onPress={onClose}>
