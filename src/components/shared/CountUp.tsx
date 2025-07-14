c9a6cf1d04737aab612a4f3b38a2a0fbd3bbb4ca
Seraph <seraph.axl@gmail.com>
2025-07-13 18:50:50 -0600

    fix: agregar soporte para diferentes tipos de conteo en el componente CountUp

--- src/components/shared/CountUp.tsx
+++ src/components/shared/CountUp.tsx
@@ -1,7 +1,29 @@
 import ReactCountUp from 'react-countup'
 
-const CountUp = ({ value }: { value: number }) => {
-  return <ReactCountUp end={value} duration={1} prefix='$' separator=',' decimals={2} decimal='.' />
+interface CountUpProps {
+  value: number
+  type?: 'currency' | 'number' | 'currency-short'
+}
+
+const CountUp = ({ value, type = 'currency' }: CountUpProps) => {
+  switch (type) {
+    case 'number':
+      return <ReactCountUp end={value} duration={1} />
+    case 'currency-short': {
+      let suffixValue
+      if (value >= 1000000) {
+        value = value / 1000000
+        suffixValue = 'M' // Retorna en millones
+      } else if (value >= 1000) {
+        value = value / 1000
+        suffixValue = 'k'
+      }
+
+      return <ReactCountUp end={value} duration={1} prefix='$' suffix={suffixValue} separator=',' decimals={1} decimal='.' />
+    }
+    default:
+      return <ReactCountUp end={value} duration={1} prefix='$' separator=',' decimals={2} decimal='.' />
+  }
 }
 
 export default CountUp
